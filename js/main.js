ac22a00d9fe21e767e0cbcfa4d5843c632bef861
Ahmed Yasser <ahmed.yassertaha@gmail.com>
2017-09-11 17:39:45 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,13 +12,13 @@
             zipCode = "Egypt/Cairo";
         }
         
-        var conditionsURL = "http://api.wunderground.com/api/745b2952d8b72680/geolookup/conditions/q/"+zipCode+".json"; 
+        var conditionsURL = "https://api.wunderground.com/api/745b2952d8b72680/geolookup/conditions/q/"+zipCode+".json"; 
         
         conditions.open('GET', conditionsURL, true);
         conditions.responseType = 'text';
         conditions.send();
 
-        var forecastURL = "http://api.wunderground.com/api/745b2952d8b72680/geolookup/forecast/q/" + zipCode + ".json";
+        var forecastURL = "https://api.wunderground.com/api/745b2952d8b72680/geolookup/forecast/q/" + zipCode + ".json";
     
         forecast.open('GET', forecastURL, true);
         forecast.responseType = 'text';
@@ -58,57 +58,3 @@
     }
     weather();
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-var weatherConditions = new XMLHttpRequest();
-var weatherForecast = new XMLHttpRequest();
-var cObj;
-var fObj;
-
-// GET THE CONDITIONS
-weatherConditions.open('', '', true);
-weatherConditions.responseType = 'text';
-weatherConditions.send(null);
-
-weatherConditions.onload = function() {
-    if (weatherConditions.status === 200){
-        cObj = JSON.parse(weatherConditions.responseText); 
-        console.log(cObj);
-
-
-    } //end if
-}; //end function
-
-
-
-// GET THE FORECARST
-weatherForecast.open('', '', true);
-weatherForecast.responseType = 'text'; 
-weatherForecast.send();
-
-weatherForecast.onload = function() {
-if (weatherForecast.status === 200){
-	fObj = JSON.parse(weatherForecast.responseText);
-	console.log(fObj);
-	
-} //end if
-}; //end function
- */
-
